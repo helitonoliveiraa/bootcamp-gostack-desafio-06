8507b78659918a3da30a18270eba822e9f50717a
Heliton Oliveira <heliton.oliveira88@gmail.com>
2020-06-17 19:15:04 -0300

    feat: page User

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,6 +1,74 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
-export default function User() {
-  return <View />;
+import api from '../../services/api';
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
+
+export default class User extends Component {
+  static navigationOptions = {
+    title: 'helo world',
+  };
+
+  static propTypes = {
+    route: PropTypes.shape().isRequired,
+  };
+
+  constructor() {
+    super();
+    this.state = {
+      stars: [],
+    };
+  }
+
+  async componentDidMount() {
+    const {route} = this.props;
+    const {user} = route.params;
+
+    const response = await api.get(`/users/${user.login}/starred`);
+
+    this.setState({stars: response.data});
+  }
+
+  render() {
+    const {route} = this.props;
+    const {stars} = this.state;
+
+    const {user} = route.params;
+
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{uri: user.avatar}} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        <Stars
+          data={stars}
+          keyExtractor={star => String(star.id)}
+          renderItem={({item}) => (
+            <Starred>
+              <OwnerAvatar source={{uri: item.owner.avatar_url}} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      </Container>
+    );
+  }
 }
